53596e4c1818a1bc9db00f2e3b762d2d22105807
guicintraa <guilhermecin26@gmail.com>
2025-08-20 20:31:36 -0300

    feat: criando componente conteudo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Header } from "./components/Header"
+import { Conteudo } from "./components/Conteudo"
 
 function App() {
  
 
   return (
-    
+      <>
       <Header/>
-    
+      <main>
+      <Conteudo title="TITULO" texto="TEXTO TEXTO TEXTO"/>
+      </main>
+    </>
   )
 }
 
